816d54d76b17716e921671f13afea51344880e43
GameDevMitchell <146736445+GameDevMitchell@users.noreply.github.com>
2024-06-03 23:07:24 +0300

    update game.py

--- game.py
+++ game.py
@@ -23,7 +23,7 @@ positions = {
 }
 
 
-# assigning of the players position
+# assigning of the players position and character
 def get_player_one():
     player_1 = easygui.enterbox("What's your name?\n You'll be player 1✖️:", "Input")
     return player_1
